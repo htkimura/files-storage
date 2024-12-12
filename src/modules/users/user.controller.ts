32dab521a7bf1528e6a941b7d5b6fd0f9f0c61df
Henry Kimura <htkimura@gmail.com>
2024-12-12 20:39:33 -0300

    feat: add user controller

---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() input: CreateUserDto) {
+    return this.userService.createUser(input);
+  }
+}
