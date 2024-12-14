f3ced1134715869b8191d39337d43f7ca5dac01c
Henry Kimura <htkimura@gmail.com>
2024-12-14 09:27:49 -0300

    feat: add login method to user service

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreateUserDto } from './dto';
-import { CreateUserUseCase } from './use-cases';
+import { CreateUserDto, LoginDto } from './dto';
+import { CreateUserUseCase, LoginUseCase } from './use-cases';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly loginUseCase: LoginUseCase,
+  ) {}
 
   createUser(input: CreateUserDto) {
     return this.createUserUseCase.execute(input);
+  }
+
+  login(input: LoginDto) {
+    return this.loginUseCase.execute(input);
   }
 }
