3457de330c4b51a9d405f330a141712c7baf19df
Henry Kimura <htkimura@gmail.com>
2024-12-29 11:09:34 -0300

    feat: add upload object route

--- src/modules/storage/storage.controller.ts
+++ src/modules/storage/storage.controller.ts
@@ -1,5 +1,15 @@
 import { AuthUser } from '@common/decorators';
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@common/guards';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { StorageService } from './storage.service';
 
@@ -8,11 +18,21 @@ export class StorageController {
   constructor(private readonly storageService: StorageService) {}
 
   @Get('objects/:objectName')
-  @UseGuards()
+  @UseGuards(AuthGuard)
   getObjectUrl(
     @AuthUser('_id') userId: string,
     @Param('objectName') objectName: string,
   ): Promise<string> {
     return this.storageService.generatePresignedUrl(`${userId}/${objectName}`);
+  }
+
+  @Post('objects')
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  uploadObject(
+    @AuthUser('_id') userId: string,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<string> {
+    return this.storageService.uploadObject(file, userId);
   }
 }
