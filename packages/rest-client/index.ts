f5e2869987ae884313cf959eb8ee7b9d96a3e206
Henry Kimura <htkimura@gmail.com>
2025-07-10 01:37:20 -0300

    chore(rest-client): refactor export and fix duplicated dependency

--- packages/rest-client/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/generated/model';
-export * from './src/generated/storage/storage';
-export * from './src/generated/user/user';
---
+++ packages/rest-client/src/index.ts
@@ -0,0 +1,3 @@
+export * from './generated/model';
+export * from './generated/storage/storage';
+export * from './generated/user/user';
