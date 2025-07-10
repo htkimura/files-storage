7abd84e4c89db09d4365052bdb675e63dab2d66d
Henry Kimura <htkimura@gmail.com>
2025-07-10 20:44:41 -0300

    feat: add GetFileByIdUseCase

---
+++ src/modules/storage/use-cases/get-file-by-id.use-case.ts
@@ -0,0 +1,38 @@
+import { FileService, FileWithPresignedUrl } from '@modules/files';
+import { UserService } from '@modules/users/user.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { R2Service } from '../r2.service';
+
+export interface GetFileByIdArgs {
+  fileId: string;
+  userId: string;
+}
+
+@Injectable()
+export class GetFileByIdUseCase {
+  constructor(
+    private readonly userService: UserService,
+    private readonly fileService: FileService,
+    private readonly r2Service: R2Service,
+  ) {}
+
+  async execute({
+    fileId,
+    userId,
+  }: GetFileByIdArgs): Promise<FileWithPresignedUrl> {
+    const foundUser = await this.userService.getUserById({ userId });
+
+    if (!foundUser) throw new NotFoundException('User not found');
+
+    const file = await this.fileService.getById(fileId);
+
+    if (file.userId !== userId) throw new NotFoundException('File not found');
+
+    const presignedUrl = await this.r2Service.generateReadPresignedUrl(
+      file.path,
+    );
+
+    return { ...file, presignedUrl };
+  }
+}
