5c2952d375c756af5d123eaaef24d42615596a30
Henry Kimura <htkimura@gmail.com>
2025-07-10 20:28:20 -0300

    refactor: stop returning presigned url on get user files

--- src/modules/users/use-cases/get-many-files-by-user-id.use-case.ts
+++ src/modules/users/use-cases/get-many-files-by-user-id.use-case.ts
@@ -1,6 +1,5 @@
-import { FileWithPresignedUrl } from '@modules/files';
+import { File } from '@modules/files';
 import { FileRepository } from '@modules/files/file.repository';
-import { StorageService } from '@modules/storage';
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { UserRepository } from '../user.repository';
@@ -14,24 +13,13 @@ export class GetManyFilesByUserIdUseCase {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly fileRepository: FileRepository,
-    private readonly storageService: StorageService,
   ) {}
 
-  async execute({ userId }: GetUserFilesArgs): Promise<FileWithPresignedUrl[]> {
+  async execute({ userId }: GetUserFilesArgs): Promise<File[]> {
     const foundUser = await this.userRepository.getById(userId);
 
     if (!foundUser) throw new NotFoundException('User not found');
 
-    const files = await this.fileRepository.getManyByUserId(userId);
-
-    const presignedUrls = await this.storageService.generateManyPresignedUrls(
-      files.map((file) => file.path),
-    );
-
-    return files.map((file, index) => {
-      const presignedUrl = presignedUrls[index];
-
-      return { ...file, presignedUrl };
-    });
+    return this.fileRepository.getManyByUserId(userId);
   }
 }
