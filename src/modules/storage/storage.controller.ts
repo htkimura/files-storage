c05579143042229d4ff849b5d9395ef9f9b27930
Henry Kimura <htkimura@gmail.com>
2025-07-10 21:01:19 -0300

    docs: storage controller endpoints

--- src/modules/storage/storage.controller.ts
+++ src/modules/storage/storage.controller.ts
@@ -1,7 +1,9 @@
 import { AuthUser } from '@common/decorators';
 import { AuthGuard } from '@common/guards';
 import { JUser } from '@common/types';
+import { FileWithPresignedUrl } from '@modules/files';
 import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { StorageService } from './storage.service';
 
@@ -11,6 +13,15 @@ export class StorageController {
 
   @Get('uploads/presigned-url')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    operationId: 'getPresignedUrl',
+    summary: 'Get presigned url to upload file',
+    description: 'Returns presigned url to upload file',
+  })
+  @ApiResponse({
+    status: 200,
+    type: String,
+  })
   getPresignedUrl(
     @Query('fileName') name: string,
     @Query('fileType') type: string,
@@ -27,12 +38,31 @@ export class StorageController {
 
   @Get('files/bulk')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    operationId: 'getBulkFilesByIds',
+    summary: 'Get the authenticated user files by ids with presigned urls',
+    description:
+      'Returns the authenticated user files by ids with presigned urls',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [FileWithPresignedUrl],
+  })
   getBulkFilesByIds(@Query('ids') ids: string[], @AuthUser() user: JUser) {
     return this.storageService.getBulkFilesByIds({ ids, userId: user._id });
   }
 
   @Get('files/:id')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    operationId: 'getFileById',
+    summary: 'Get file by id with presigned url',
+    description: 'Returns file by id with presigned url',
+  })
+  @ApiResponse({
+    status: 200,
+    type: FileWithPresignedUrl,
+  })
   getFileById(@Param('id') id: string, @AuthUser() user: JUser) {
     return this.storageService.getFileById({ fileId: id, userId: user._id });
   }
