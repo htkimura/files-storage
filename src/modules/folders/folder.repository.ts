08bfc1b6eb634553d4bea6c4ff7739cf1a47642c
Henry Kimura <htkimura@gmail.com>
2026-01-10 17:45:01 -0300

    feat: add folder repository

---
+++ src/modules/folders/folder.repository.ts
@@ -0,0 +1,53 @@
+import { PrismaService } from '@modules/prisma';
+import { Injectable } from '@nestjs/common';
+
+import { Folder } from './folder.model';
+
+interface CreateFolderInput
+  extends Omit<Folder, 'id' | 'createdAt' | 'updatedAt'> {
+  id?: string;
+}
+
+interface UpdateFolderInput
+  extends Partial<Omit<Folder, 'id' | 'createdAt' | 'updatedAt'>> {}
+
+@Injectable()
+export class FolderRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(data: CreateFolderInput): Promise<Folder> {
+    const now = new Date();
+
+    return this.prismaService.folder.create({
+      data: {
+        ...data,
+        createdAt: now,
+        updatedAt: now,
+      },
+    });
+  }
+
+  getById(folderId: string): Promise<Folder> {
+    return this.prismaService.folder.findUnique({
+      where: { id: folderId },
+    });
+  }
+
+  update(id: string, data: UpdateFolderInput): Promise<Folder> {
+    const now = new Date();
+
+    return this.prismaService.folder.update({
+      data: {
+        ...data,
+        updatedAt: now,
+      },
+      where: { id },
+    });
+  }
+
+  deleteById(folderId: string): Promise<Folder> {
+    return this.prismaService.folder.delete({
+      where: { id: folderId },
+    });
+  }
+}
