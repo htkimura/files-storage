a434f654ae17387c70b3fed1d2de1b5c3aa73f20
Henry Kimura <htkimura@gmail.com>
2026-09-14 12:25:49 -0300

    chore: add seekmemo.com domain to cors list

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'https://my-files-ten.vercel.app',
+      'https://my-files-ten.vercel.app',
+      'https://seekmemo.com',
     ],
   });
 
